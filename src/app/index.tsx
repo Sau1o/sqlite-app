14f8c758f3b6b8d71dbc8ee901b8444eb18b5e41
Saulo Coelho <coelho.saulo@gmail.com>
2025-01-07 21:28:33 -0300

    feat:added function to insert products

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,35 @@
-import {View, Text} from 'react-native'
+import { Input } from '@/components/Input'
+import { useState } from 'react'
+import {View, Button, Alert} from 'react-native'
+
+import {useProductDatabase} from "@/database/useProductDatabase"
 
 export default function Index(){
+    const [id,setId] = useState("")
+    const [name,setName] = useState("")
+    const [quantity, setQuantity] = useState("")
+    const [products, setProducts] = useState([])
+
+    const productDatabase = useProductDatabase()
+
+    async function create(){
+        try{
+            if(isNaN(Number(quantity))){
+               return Alert.alert("Quantidade","A quantidade precisa ser um numero")
+            }
+        const response = await productDatabase.create({name, quantity:Number(quantity)})
+        Alert.alert("Produto cadastrado com o ID: " + response.insertedRowId)
+        } catch(error){
+            console.log(error)
+        }
+    }
+
     return (
-    <View style={{flex: 1, justifyContent:"center", alignItems:"center"}}>
-        <Text>Teste!</Text>
+    <View style={{flex: 1, justifyContent:"center", padding: 32, gap: 16}}>
+        <Input placeholder='Nome' onChangeText={setName} value={name}/>
+        <Input placeholder='Quantidade' onChangeText={setQuantity} value={quantity}/>
+
+        <Button title="Salvar" onPress={create}/>
     </View>
     )
 }
---
+++ src/components/Input.tsx
@@ -0,0 +1,12 @@
+import { TextInput, TextInputProps } from "react-native";
+
+export function Input({...rest}: TextInputProps){
+    return <TextInput style={{
+        height:54, 
+        borderWidth: 1, 
+        borderBlockColor:"#999",
+        borderRadius: 7,
+        paddingHorizontal: 16,
+    }} 
+    {...rest}/>
+}
