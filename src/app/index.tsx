03b2997e9293712df7f8a4c95f68488325805829
Saulo Coelho <coelho.saulo@gmail.com>
2025-01-12 18:36:13 -0300

    feat:add numeric type keyboard

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,83 +1,83 @@
 import { Input } from '@/components/Input'
 import { useState, useEffect } from 'react'
-import {View, Button, Alert, FlatList} from 'react-native'
+import { View, Button, Alert, FlatList } from 'react-native'
 import { router } from 'expo-router'
 
-import {useProductDatabase, ProductDatabase} from "@/database/useProductDatabase"
+import { useProductDatabase, ProductDatabase } from "@/database/useProductDatabase"
 import { Product } from '@/components/Producti'
 
-export default function Index(){
-    const [id,setId] = useState("")
-    const [name,setName] = useState("")
-    const [search,setSearch] = useState("")
+export default function Index() {
+    const [id, setId] = useState("")
+    const [name, setName] = useState("")
+    const [search, setSearch] = useState("")
     const [quantity, setQuantity] = useState("")
     const [products, setProducts] = useState<ProductDatabase[]>([])
 
     const productDatabase = useProductDatabase()
 
-    async function create(){
-        try{
-            if(isNaN(Number(quantity))){
-               return Alert.alert("Quantidade","A quantidade precisa ser um numero")
+    async function create() {
+        try {
+            if (isNaN(Number(quantity))) {
+                return Alert.alert("Quantidade", "A quantidade precisa ser um numero")
             }
-        const response = await productDatabase.create({name, quantity:Number(quantity)})
+            const response = await productDatabase.create({ name, quantity: Number(quantity) })
 
-        Alert.alert("Produto cadastrado com o ID: " + response.insertedRowId)
-        } catch(error){
+            Alert.alert("Produto cadastrado com o ID: " + response.insertedRowId)
+        } catch (error) {
             console.log(error)
         }
     }
 
-    async function update(){
-        try{
-            if(isNaN(Number(quantity))){
-               return Alert.alert("Quantidade","A quantidade precisa ser um numero")
+    async function update() {
+        try {
+            if (isNaN(Number(quantity))) {
+                return Alert.alert("Quantidade", "A quantidade precisa ser um numero")
             }
-        const response = await productDatabase.update({
-            id:Number(id),
-            name, 
-            quantity:Number(quantity),
-        })
+            const response = await productDatabase.update({
+                id: Number(id),
+                name,
+                quantity: Number(quantity),
+            })
 
-        Alert.alert("Produto Atualizado.")
-        } catch(error){
+            Alert.alert("Produto Atualizado.")
+        } catch (error) {
             console.log(error)
         }
-    } 
+    }
 
     async function list() {
-        try{
+        try {
             const response = await productDatabase.searchByName(search)
             setProducts(response)
-        }catch{
+        } catch {
 
         }
     }
 
-    async function remove(id:number){
-        try{
+    async function remove(id: number) {
+        try {
             await productDatabase.remove(id)
             setId("")
             setName("")
             setQuantity("")
             await list()
             Alert.alert("Produto Removido com Sucesso.")
-        }catch(error){
+        } catch (error) {
             console.log(error)
         }
 
     }
 
-    function details (item: ProductDatabase){
+    function details(item: ProductDatabase) {
         setName(item.name)
         setQuantity(String(item.quantity))
         setId(String(item.id))
     }
 
-    async function handleSave(){
-        if(id){
+    async function handleSave() {
+        if (id) {
             update()
-        }else{
+        } else {
             create()
         }
 
@@ -87,32 +87,32 @@ export default function Index(){
         await list()
     }
 
-    useEffect(()=> {
+    useEffect(() => {
         list()
-    },[search])
+    }, [search])
 
     return (
-    <View style={{flex: 1, justifyContent:"center", padding: 32, gap: 16}}>
-        <Input placeholder='Nome' onChangeText={setName} value={name}/>
-        <Input placeholder='Quantidade' onChangeText={setQuantity} value={quantity}/>
+        <View style={{ flex: 1, justifyContent: "center", padding: 32, gap: 16 }}>
+            <Input placeholder='Nome' onChangeText={setName} value={name} />
+            <Input placeholder='Quantidade' onChangeText={setQuantity} value={quantity} keyboardType='numeric' />
 
-        <Button title={id ? "Atualizar" : "Salvar"} onPress={handleSave}/>
+            <Button title={id ? "Atualizar" : "Salvar"} onPress={handleSave} />
 
-        <Input placeholder='Pesquisar' onChangeText={setSearch} />
+            <Input placeholder='Pesquisar' onChangeText={setSearch} />
 
-        <FlatList 
-            data={products}
-            keyExtractor={(item)=>String(item.id)}
-            renderItem={({item})=>(
-                <Product   
-                    data={item} 
-                    onPress={() => details(item)} 
-                    onDelete={()=>remove(item.id)}
-                    onOpen={() => router.navigate("/details/" + item.id)}
-                />
-            )}
-            contentContainerStyle={{gap: 16}}
-        />
-    </View>
+            <FlatList
+                data={products}
+                keyExtractor={(item) => String(item.id)}
+                renderItem={({ item }) => (
+                    <Product
+                        data={item}
+                        onPress={() => details(item)}
+                        onDelete={() => remove(item.id)}
+                        onOpen={() => router.navigate("/details/${id}")}
+                    />
+                )}
+                contentContainerStyle={{ gap: 16 }}
+            />
+        </View>
     )
 }
